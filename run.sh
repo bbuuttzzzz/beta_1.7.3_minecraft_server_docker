c7039d5cd96a2fe4e423aea3aea4eeab25fddf44
Drew <pantsrain@gmail.com>
2024-12-14 22:32:55 -0600

    run.sh fiddling

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 docker run -t \
     -p 25565:25565/udp \
     -p 25565:25565 \
-    -v /root/mount/server.properties \
-    -v /root/mount/world \
+    -v /root/mount/server.properties:/app/server.properties \
+    -v /root/mount/world:/app/world \
     --name minecraft mcserv
